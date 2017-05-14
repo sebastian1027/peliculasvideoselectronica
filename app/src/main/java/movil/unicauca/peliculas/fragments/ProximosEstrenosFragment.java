0dacb97461702737a49263ff606da1ebcaa4a322
Sebastian <sebastianl@unicauca.edu.co>
2017-05-14 16:49:59 -0500

    extraccion de dimensiones implemetadas

--- app/src/main/java/movil/unicauca/peliculas/fragments/ProximosEstrenosFragment.java
+++ app/src/main/java/movil/unicauca/peliculas/fragments/ProximosEstrenosFragment.java
@@ -49,46 +49,46 @@ public class ProximosEstrenosFragment extends Fragment implements ProximosEstren
     private void loadDatape(){
 
         ProximosEstrenos pe1 = new ProximosEstrenos();
-        pe1.setNombre("Colossal");
-        pe1.setFecha_estreno("18 de Mayo");
-        pe1.setImagen("http://www.asturscore.com/wp-content/uploads/2017/04/Colossal.jpg");
-        pe1.setDuracion("110 minutos");
+        pe1.setNombre(getString(R.string.colossal));
+        pe1.setFecha_estreno(getString(R.string.mayo18));
+        pe1.setImagen(getString(R.string.imgcolosal));
+        pe1.setDuracion(getString(R.string.minutos110));
 
         ProximosEstrenos pe2 = new ProximosEstrenos();
-        pe1.setNombre("Mamá");
-        pe1.setFecha_estreno("18 de Mayo");
-        pe1.setImagen("https://s-media-cache-ak0.pinimg.com/736x/55/07/df/5507df006cb8451be78d704df99ae93e.jpg");
-        pe1.setDuracion("78 minutos");
+        pe1.setNombre(getString(R.string.mom));
+        pe1.setFecha_estreno(getString(R.string.mayo18));
+        pe1.setImagen(getString(R.string.imgmom));
+        pe1.setDuracion(getString(R.string.minutos78));
 
         ProximosEstrenos pe3 = new ProximosEstrenos();
-        pe1.setNombre("Huye!");
-        pe1.setFecha_estreno("18 de Mayo");
-        pe1.setImagen("http://www.cinevistablog.com/wp-content/uploads/2017/04/huye-get-out-pelicula-de-horror-y-misterio-exito-de-taquilla-y-en-sundance_opt2_.jpg");
-        pe1.setDuracion("1 hora 44 minutos");
+        pe1.setNombre(getString(R.string.huye));
+        pe1.setFecha_estreno(getString(R.string.mayo18));
+        pe1.setImagen(getString(R.string.imghuye));
+        pe1.setDuracion(getString(R.string.unahora44min));
 
         ProximosEstrenos pe4 = new ProximosEstrenos();
-        pe1.setNombre("Rapido y Peludo");
-        pe1.setFecha_estreno("1 de Junio");
-        pe1.setImagen("http://p2.trrsf.com/image/fget/cf/thumbox-ttv.terra.com/video/58dab9ce2735ba1f2f8d0fea.jpg");
-        pe1.setDuracion("90 minutos");
+        pe1.setNombre(getString(R.string.rapidoypeludo));
+        pe1.setFecha_estreno(getString(R.string.junio1));
+        pe1.setImagen(getString(R.string.imgrapidoypeludo));
+        pe1.setDuracion(getString(R.string.minutos90));
 
         ProximosEstrenos pe5 = new ProximosEstrenos();
-        pe1.setNombre("La Mujer Maravilla");
-        pe1.setFecha_estreno("1 de Junio");
-        pe1.setImagen("http://media.ultimahora.com/adjuntos/161/imagenes/003/328/0003328988.jpg");
-        pe1.setDuracion("120 minutos");
+        pe1.setNombre(getString(R.string.mujermaravilla));
+        pe1.setFecha_estreno(getString(R.string.junio1));
+        pe1.setImagen(getString(R.string.imgmujermaravilla));
+        pe1.setDuracion(getString(R.string.minutos120));
 
         ProximosEstrenos pe6 = new ProximosEstrenos();
-        pe1.setNombre("La Momia");
-        pe1.setFecha_estreno("8 de Junio");
-        pe1.setImagen("https://1.bp.blogspot.com/-bfnPczu2DhM/WEU_M1EdIfI/AAAAAAABD1I/FuYrO24IMsAEWcVtUGIB5DQGSnMSblsVACLcB/s1600/trailer-momia-2017.jpg");
-        pe1.setDuracion("120 Minutos");
+        pe1.setNombre(getString(R.string.lamomia));
+        pe1.setFecha_estreno(getString(R.string.junio8));
+        pe1.setImagen(getString(R.string.imglamomia));
+        pe1.setDuracion(getString(R.string.minutos120));
 
         ProximosEstrenos pe7 = new ProximosEstrenos();
-        pe1.setNombre("Guardianes de la Bahia");
-        pe1.setFecha_estreno("15 de Junio");
-        pe1.setImagen("https://cinemedios.files.wordpress.com/2016/12/baywatch-2017-cast-trailer-article.png");
-        pe1.setDuracion("120 Minutos");
+        pe1.setNombre(getString(R.string.guardianesbahia));
+        pe1.setFecha_estreno(getString(R.string.junio15));
+        pe1.setImagen(getString(R.string.imggaurdianesbahia));
+        pe1.setDuracion(getString(R.string.minutos120));
 
         Datape.data.add(pe1);
         Datape.data.add(pe2);
